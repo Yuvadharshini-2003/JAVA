7df474776d8df8e15b937c37aa6df80f0b846e8c
Yuvadharshini-2003 <154663369+Yuvadharshini-2003@users.noreply.github.com>
2024-07-11 20:42:26 +0530

    Add files via upload

---
+++ JAVA/Round_Off.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    float n=sc.nextFloat();
+    System.out.println((int)n);
+    System.out.println((int)Math.ceil(n));
+    System.out.println((int)Math.floor(n));
+  }
+}
